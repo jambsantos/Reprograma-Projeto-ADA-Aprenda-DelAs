6926d22a0e0e919d8e6b562c57eb326f66db08c8
jambsantos <jessica.aparecida@gmail.com>
2020-12-06 10:10:44 -0300

    model users concluida

---
+++ scr/models/users.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { coursesSchema } = require('./cursos');
+
+const usersSchema = new mongoose.Schema({
+  id:{ type: Number },
+  nome: { type: String },
+  tagArea:{ type: String },
+  contacts: { type: Number },
+  email: { type: String },
+  senha: { type: String },
+  cursos: [coursesSchema]
+}, {
+  versionKey: false
+});
+
+const usersModel = mongoose.model('users', usersSchema);
+
+module.exports = { usersModel, usersSchema };
